b1568a3ae25459fcbbc750546876858c8e7395ae
satyamsinha111 <satyamsinha8158@gmail.com>
2020-12-02 11:18:28 +0530

    authentication added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 const Stack = createStackNavigator();
 import Screens from './screens';
+import {useDispatch, connect} from 'react-redux';
+import auth from '@react-native-firebase/auth';
+import {IS_AUTHENTICATED} from './actions/action.type';
 
-const App = () => {
+const App = ({authState}) => {
+  const dispatch = useDispatch();
+  const onStateChanged = (user) => {
+    //On state changed
+    if (user) {
+      console.log('User is authenticated');
+      dispatch({
+        type: IS_AUTHENTICATED,
+        payload: true,
+      });
+    } else {
+      console.log('User is not authenticated');
+      dispatch({
+        type: IS_AUTHENTICATED,
+        payload: false,
+      });
+    }
+  };
+
+  useEffect(() => {
+    console.log(authState);
+    const subscriber = auth().onAuthStateChanged(onStateChanged);
+    return subscriber;
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Screens.Login} />
-        <Stack.Screen name="Register" component={Screens.Register} />
-        <Stack.Screen name="Home" component={Screens.Home} />
-        <Stack.Screen name="AddNote" component={Screens.AddNote} />
-        <Stack.Screen name="EditNote" component={Screens.EditNote} />
+        {!authState.isAuthenticated ? (
+          <>
+            <Stack.Screen name="Login" component={Screens.Login} />
+            <Stack.Screen name="Register" component={Screens.Register} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Home" component={Screens.Home} />
+            <Stack.Screen name="AddNote" component={Screens.AddNote} />
+            <Stack.Screen name="EditNote" component={Screens.EditNote} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-export default App;
+const mapStateToProps = (state) => ({
+  authState: state.auth,
+});
+
+export default connect(mapStateToProps)(App);
--- src/store.js
+++ src/store.js
@@ -2,7 +2,6 @@ import {createStore, applyMiddleware} from 'redux';
 import RootReducer from './reducers';
 import thunk from 'redux-thunk';
 const middleware = [thunk];
-
 import {composeWithDevTools} from 'redux-devtools-extension';
 
 const store = createStore(
